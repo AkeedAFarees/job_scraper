896353a1cf18a79217bce2a30b671ec46ebaea37
Akeed A Farees <akeedahmedfarees@gmail.com>
2022-02-07 15:50:09 +0500

    Remove file related code

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -7,10 +7,8 @@ class RecordsController < ApplicationController
   def create
     if params[:record][:skills].present?
       Record.scrape(params[:record][:skills])
-      puts "!!!!!!!!!!!!!!!!!!!!!!!"
     else
       Record.import(params[:record][:file])
-      puts "&&&&&&&&&&&&&&&&&&&&&&&"
       # flash[:notice] = "Records uploaded successfully"
     end
     redirect_to records_path
