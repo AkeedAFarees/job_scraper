a468478decabd5e0d772b435d7eef06c5dc4f5ce
Akeed A Farees <akeedahmedfarees@gmail.com>
2022-02-16 17:39:35 +0500

    Add env variables to database.yml and add host to config

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module WebScraper
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.hosts << "job-scrape.cap.mikaels.com"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
