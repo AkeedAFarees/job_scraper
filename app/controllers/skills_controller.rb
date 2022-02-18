928ed637b63abb1e0c3797fbe34a76e82fd08194
Akeed A Farees <akeedahmedfarees@gmail.com>
2022-02-18 18:49:35 +0500

    Add controller module

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -35,6 +35,17 @@ class SkillsController < ApplicationController
     @skill.destroy!
   end
 
+  def calculate_form
+    @skills = Skill.all
+  end
+
+  def calculate
+    charge = 100.00
+
+    @skill = Skill.find_by(name: skill_params[:name], experience: skill_params[:experience])
+    @total = @skill.present? ? @skill.max * charge : nil
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_skill
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
-  resources :skills
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   root 'sessions#create'
 
-  resources :users, :records
+  resources :users, :records, :skills
 
   get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#destroy'
   get '/logout', to: 'sessions#destroy'
 
   get '/scraper', to: 'scraper#index'
-
   get '/export', to: 'records#export'
 
   post 'password/forgot', to: 'password#forgot'
@@ -21,5 +16,7 @@ Rails.application.routes.draw do
   put 'password/update', to: 'password#update'
   get 'password/edit', to: 'password#edit'
 
+  get '/calculate_form', to: 'skills#calculate_form'
+  post '/calculate', to: 'skills#calculate'
 
 end
