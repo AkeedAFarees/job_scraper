10f79a20b6346c29ccf711d23479801d4b4b461d
Akeed A Farees <akeedahmedfarees@gmail.com>
2022-02-21 19:33:53 +0500

    Revert css changes - Remove imports from javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import "bootstrap"
-import "/app/assets/stylesheets/application.css.scss"
+// import "bootstrap"
+// import "/app/assets/stylesheets/application.css.scss"
 
 Rails.start()
 Turbolinks.start()
